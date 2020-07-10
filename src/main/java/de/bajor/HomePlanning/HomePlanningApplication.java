986d987007df95fad65e5bdb3df1a0511099bfdc
bbajor <19325120+bbajor@users.noreply.github.com>
2020-07-10 07:43:00 +0200

    Initial Spring project

---
+++ src/main/java/de/bajor/HomePlanning/HomePlanningApplication.java
@@ -0,0 +1,13 @@
+package de.bajor.HomePlanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bajor/HomePlanning/HomePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bajor.HomePlanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
